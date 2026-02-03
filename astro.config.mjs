dc0b19bca2d4e75c96cb3aa99ea65c71791e1511
JamesVanDyk <jvandyk1@IvyTech.edu>
2026-02-03 18:21:33 +0000

    changed base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	base: "/repo-name", // replace 'repo-name' with your GitHub repository name
+	base: "/astro-website-lecture-activity", // replace 'repo-name' with your GitHub repository name
 });
